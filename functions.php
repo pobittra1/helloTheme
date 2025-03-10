9c6ce37156de97e002edb7d8443b1e2a91706fa9
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2025-03-10 17:01:30 +0600

    updated

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ function my_theme_setup()
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     register_nav_menus(array(
-        'primary' => __('Primary Menu', 'my-theme'),
+        'primary' => __('Primary Menu', 'hello-theme'),
     ));
 }
-add_action('after_setup_theme', 'my_theme_setup');
+add_action('after_setup_theme', 'hello_theme_setup');
 
 
 require_once get_template_directory() . '/inc/enqueue.php';
